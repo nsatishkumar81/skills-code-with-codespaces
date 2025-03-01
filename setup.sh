2647a5851421ab7b618456ae4d6b2f8f6a478a15
nsatishkumar81 <60126716+nsatishkumar81@users.noreply.github.com>
2025-03-01 05:38:51 +0000

    Adding setup.sh from the codespace!

--- setup.sh
+++ setup.sh
@@ -2,5 +2,4 @@
 
 sudo apt-get update
 sudo apt-get install sl
-echo "export PATH=\$PATH:/usr/games" >> ~/.bashrcgit add setup.sh --chmod=+x
-git commit -m "Adding setup.sh from the codespace!"
+echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
